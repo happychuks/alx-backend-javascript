a7e38fcd5549af8460ade05ae65c88537233c6a3
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-09-19 10:25:11 +0000

    Task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Happy Chukwuma <https://github.com/happychuks>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
